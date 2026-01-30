a14a0d6a865ab03e8b1b59bde5976722051649eb
Gebremikial <gebremikialgebrie@gmail.com>
2026-01-30 09:26:42 +0300

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import ContentOne from './components/ContentOne';
 import ContentTwo from './components/ContentTwo';
-import ContentThree from './components/ContentThree';
+import ContentThree from './components/ContentThree'; 
 
 function App() {
   return (
-    <div className="p-10 space-y-6">
+    
+    <div className="flex flex-col min-h-screen">
+      
       <Navbar />
-      <ContentOne />
-      <ContentTwo />
-      <ContentThree />
+      <main className="flex-grow">
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    
+          <div className="space-y-6">
+            <ContentOne />
+            <ContentTwo />
+            <ContentThree />
+          </div>
+
+        </div>
+      </main>
+
+      <Footer />
+      
     </div>
   );
 }
